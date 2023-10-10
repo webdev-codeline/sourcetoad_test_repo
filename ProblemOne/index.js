35b1543c8764a15c74221df4b3b245995635eb50
Sean Francis <polaris@vmi1316688.contaboserver.net>
2023-10-10 10:44:49 -0700

    initial commit and build the flatten object function and implemented in Problem1

---
+++ ProblemOne/index.js
@@ -0,0 +1,80 @@
+var arr = [
+  {
+    'guest_type': 'crew',
+    'first_name': 'Marco',
+    'last_name': 'Burns',
+    'guest_booking': {
+      'room_no': 'A0073',
+      'some_array': [7, 2, 4]
+    },
+  },
+  {
+    'guest_type': 'guest',
+    'first_name': 'John',
+    'last_name': 'Doe',
+    'guest_booking': {
+      'room_no': 'C73',
+      'some_array': [1, 3, 5, 2, 4, 3]
+    },
+  },
+  {
+    'guest_type': 'guest',
+    'first_name': 'Jane',
+    'last_name': 'Doe',
+    'guest_booking': {
+      'room_no': 'C73',
+      'some_array': [1, 3, 5, 2, 4, 3]
+    },
+  },
+  {
+    'guest_type': 'guest',
+    'first_name': 'Albert',
+    'last_name': 'Einstein',
+    'guest_booking': {
+      'room_no': 'B15',
+      'some_array': [2, 5, 6, 3]
+    },
+  },
+  {
+    'guest_type': 'crew',
+    'first_name': 'Jack',
+    'last_name': 'Daniels',
+    'guest_booking': {
+      'room_no': 'B15',
+      'some_array': [2, 5, 6, 3]
+    },
+  },
+  {
+    'guest_type': 'guest',
+    'first_name': 'Alan',
+    'last_name': 'Turing',
+    'guest_booking': {
+      'room_no': 'B15',
+      'some_array': [2, 5, 6, 3]
+    },
+  },
+];
+
+function flattenObject(obj, result = {}) {
+  for (let key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      if (typeof obj[key] === 'object' && obj[key] !== null && !Array.isArray(obj[key])) {
+        flattenObject(obj[key], result);
+      } else {
+        result[key] = obj[key];
+      }
+    }
+  }
+  return result;
+}
+
+
+function mutateArray(a) {
+
+  return a.map(el => flattenObject(el));
+}
+
+$(document).ready(function () {
+  $('#originalArray').html(JSON.stringify(arr, null, 2));
+  $('#resultsArray').html(JSON.stringify(mutateArray(arr), null, 2));
+});
