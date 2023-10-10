b70ae319bc50a3e3c13bf006ee74fad21430db53
webdev-codeline <rain116093@hotmail.com>
2023-10-10 11:43:30 -0700

    initialize expo project

---
+++ ProblemTwo/simple_calc/App.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, SafeAreaView } from 'react-native';
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
